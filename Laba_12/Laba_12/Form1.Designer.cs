d20583becd01133c0f871a5989acb14731719a12
Vlad <150374872+Lesnoy012@users.noreply.github.com>
2024-12-16 22:30:21 +0300

    Laba #13 "SortDirectSelection"

--- Laba_12/Laba_12/Form1.Designer.cs
+++ Laba_12/Laba_12/Form1.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.buttonSort = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.CheckBox = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.SortMethod = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Comparisons = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Permutations = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown_arraySize = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_arraySize)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -56,48 +56,6 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(685, 212);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // buttonSort
-            // 
-            this.buttonSort.Location = new System.Drawing.Point(9, 240);
-            this.buttonSort.Name = "buttonSort";
-            this.buttonSort.Size = new System.Drawing.Size(204, 59);
-            this.buttonSort.TabIndex = 1;
-            this.buttonSort.Text = "Сортировать";
-            this.buttonSort.UseVisualStyleBackColor = true;
-            this.buttonSort.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(279, 240);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 18);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Размер массива";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(487, 240);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(204, 59);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Выход";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(223, 268);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            10000,
-            0,
-            0,
-            0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(249, 20);
-            this.numericUpDown1.TabIndex = 4;
             // 
             // CheckBox
             // 
@@ -139,13 +97,60 @@
             this.Time.ReadOnly = true;
             this.Time.Width = 150;
             // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(50, 242);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(105, 28);
+            this.buttonSort.TabIndex = 1;
+            this.buttonSort.Text = "Сортировать";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(277, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Размер массива";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // numericUpDown_arraySize
+            // 
+            this.numericUpDown_arraySize.Location = new System.Drawing.Point(225, 248);
+            this.numericUpDown_arraySize.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDown_arraySize.Name = "numericUpDown_arraySize";
+            this.numericUpDown_arraySize.Size = new System.Drawing.Size(249, 20);
+            this.numericUpDown_arraySize.TabIndex = 4;
+            this.numericUpDown_arraySize.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(700, 308);
-            this.Controls.Add(this.numericUpDown1);
+            this.ClientSize = new System.Drawing.Size(700, 283);
+            this.Controls.Add(this.numericUpDown_arraySize);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonSort);
@@ -154,7 +159,7 @@
             this.Text = "Ayupov 23VP1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_arraySize)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,7 +171,7 @@
         private System.Windows.Forms.Button buttonSort;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown_arraySize;
         private System.Windows.Forms.DataGridViewCheckBoxColumn CheckBox;
         private System.Windows.Forms.DataGridViewTextBoxColumn SortMethod;
         private System.Windows.Forms.DataGridViewTextBoxColumn Comparisons;
--- Laba_12/Laba_12/Form1.cs
+++ Laba_12/Laba_12/Form1.cs
@@ -13,14 +13,12 @@ namespace Laba_12
     public partial class Form1 : Form
     {
         String[] arraySortNames = { "Обмен", "Выбор", "Включение", "Шелла", "Быстрая", "Линейная", "Встроенная" };
+        int arraySize;
+        int[] array;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -28,9 +26,59 @@ namespace Laba_12
             dataGridView1.RowCount = 7;
             for (int i = 0; i < dataGridView1.RowCount; i++)
             {
-                dataGridView1.Rows[i].Cells[0].Value = true;
                 dataGridView1.Rows[i].Cells[1].Value = arraySortNames[i];
             }
+        }
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            arraySize = (int)numericUpDown_arraySize.Value;
+            array = new int[arraySize];
+            int time = 4;
+            int permutations = 3;
+            int comparisons = 2;
+            for (int i = 0; i < array.Length; i++) array[i] = rnd.Next(1, 10);
+            InfoSort sortDirectSelection = SortDirectSelection(array);
+            dataGridView1.Rows[1].Cells[time].Value = sortDirectSelection.Time;
+            dataGridView1.Rows[1].Cells[permutations].Value = sortDirectSelection.Permutations;
+            dataGridView1.Rows[1].Cells[comparisons].Value = sortDirectSelection.Comparisons;
+        }
+
+        public InfoSort SortDirectSelection(int[] _arr)
+        {
+            int comparisons = 0;
+            int permutations = 0;
+
+            int[] arr = new int[_arr.Length]; // Создаётся копия массива, дабы не сортировать исходный.
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = _arr[i];
+            }
+
+            int StartTime = Environment.TickCount;
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    int minIndex = i; // Индекс минимального элемента в неотсортированной части массива.
+
+                    for (int j = i + 1; j < arr.Length; j++)  // Поиск минимального элемента в неотсортированной части массива
+                    {
+                        if (arr[j] < arr[minIndex])
+                        {
+                            minIndex = j;
+                            comparisons++;
+                        }
+                    }
+                    
+                    // Swap.
+                    int temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                    permutations++; 
+                }
+            }
+            int ResultTime = Environment.TickCount - StartTime;
+            return new InfoSort { Time = ResultTime, Comparisons = comparisons,  Permutations = permutations};
         }
 
         private void button1_Click_1(object sender, EventArgs e)
